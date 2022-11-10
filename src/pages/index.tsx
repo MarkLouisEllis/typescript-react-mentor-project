4b2d8b17453dce538f1783471f581cd861171a54
Mark Ellis <mark@mohara.co>
2022-11-10 08:59:30 +0200

    imported component into a page with props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import FirstComponent from "../components/first-component";
 
 const FirstPage = () => {
-	return <text>this is the first page</text>;
+  return (
+    <>
+      <div>
+        <header>This is the first page with a functional component</header>
+      </div>
+      <div>
+        <p>This is a functional component:</p>
+        <FirstComponent
+          text="This is the props for the first component"
+        />
+      </div>
+    </>
+  )
 };
 
 export default FirstPage;
